3b1d9747a1c038bdd53bc4be67e0c9b74d0908c1
C Martin <cmart131@yahoo.com>
2023-05-12 16:07:59 -0700

    fixed styling on contact page. still need to figure out correct functionality of the form submission

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,4 +1,7 @@
 import './contact.css';
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import React, { useRef, useState } from 'react';
 import { Row, Col } from "react-bootstrap";
@@ -8,13 +11,20 @@ import {
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
-import { MdOutlineEmail } from 'react-icons/md';
+// import { MdOutlineEmail } from 'react-icons/md';
 import emailjs from '@emailjs/browser';
 
 const Contact = () => {
-  const [message, setMessage] = useState(false);
+  const [fullName, setFullName] = useState('')
+  const [email, setEmail] = useState('')
+  const [textArea, setTextArea] = useState('')
+
+  const [err, setErr] = useState(false)
+  const [ setMessage] = useState(false);
   const formRef = useRef();
+
   const handleSubmit = (e) => {
+
     e.preventDefault();
     setMessage(true);
     emailjs
@@ -34,12 +44,16 @@ const Contact = () => {
       );
 
     e.target.reset();
+    e.preventDefault()
+    if(!fullName || !email || !textArea) {
+      setErr(true)
   };
+}
   return (
     <section id="contact">
       <Row>
           <Col md={12} className="home-about-social">
-            <h1>FIND ME ON</h1>
+            <h1 className="heading">FIND ME ON</h1>
 
             <ul className="home-about-social-links">
               <li className="social-icons">
@@ -77,49 +91,45 @@ const Contact = () => {
             </ul>
           </Col>
         </Row>
-<br>
-</br>
 <br></br>
       <h3>
       Have an interesting project or want to know more about me? I'd love to hear from you!
       </h3>
-      <h3>
+      <h3 style={{ textAlign: "center" }}>
       Get in touch!
       </h3>
-      <div className="container contact__container">
-        <div className="contact__options">
-          <article className="contact__option">
-            <MdOutlineEmail className="contact__option-icon" />
-            <h4>Email</h4>
-            <a href="mailto:cmart131@yahoo.com">Send a message</a>
-          </article>
-        </div>
-        <form ref={formRef} onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder="Your Full Name"
-            name="user_name"
-            required
-          />
-          <input
-            type="text"
-            placeholder="Your Email"
-            name="user_email"
-            required
-          />
-          <textarea
-            placeholder="Your message"
-            rows="7"
-            name="message"
-            required
-          ></textarea>
-          <button type="submit" className="btn btn-primary"><a href="mailto:cmart131@yahoo.com">Send message</a>
-            </button>
-          {message && <span>Thank you!</span>}
-        </form>
-      </div>
-    </section>
+      <br></br>
+      <br></br>
+  <div className="container contact__container">
+      <Form onSubmit={handleSubmit}>
+      <div className="mb-3">
+  <label htmlFor="exampleFormControlInput1" className="form-label">Full Name</label>
+  <input type="text" onChange={(e) => setFullName(e.target.value)} value={fullName} className="form-control" id="exampleFormControlInput1" placeholder="Full Name" />
+  {err && !fullName &&
+      <p>* You must include full name</p>
+      }
+</div>
+      <div className="mb-3">
+  <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+  <input type="email" onChange={(e) => setEmail(e.target.value)} value={email} className="form-control" id="exampleFormControlInput2" placeholder="name@example.com" />
+  {err && !email &&
+      <p>* You must include an Email</p>
+      }
+</div>
+<div className="mb-3">
+  <label htmlFor="exampleFormControlTextarea1" className="form-label">Message</label>
+  <textarea className="form-control" onChange={(e) => setTextArea(e.target.value)} value={textArea} id="exampleFormControlTextarea1" rows="3"></textarea>
+  {err && !textArea &&
+      <p>* You must include a descriptive message</p>
+      }
+</div>
+    <Button type="submit" className="button">Submit</Button>
+      </Form>
+
+  </div>
+</section>
   );
 };
+
 
 export default Contact;
